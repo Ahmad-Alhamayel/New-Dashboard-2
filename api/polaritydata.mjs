67e98b44d00cfb244bb6d910f097ac3e35f48d58
unknown <ahamayel3@gmail.com>
2024-11-02 15:06:13 +0300

    fixed lambda.

--- api/polaritydata.mjs
+++ api/polaritydata.mjs
@@ -31,7 +31,7 @@ export async function GET(request) {
         "Content-Type": "application/json",
       },
     });
-  return new Response(data, {
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
